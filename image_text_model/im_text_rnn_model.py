ba6cdc786404aedf89662ede78e870922406e809
Arnav Kumar <arnav@arnavkumar.com>
2018-11-30 17:22:04 -0500

    Modify model code to work with our data and generate proper output

--- image_text_model/im_text_rnn_model.py
+++ image_text_model/im_text_rnn_model.py
@@ -1,5 +1,9 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 import os
 import sys
+import csv
 
 import numpy as np
 import pandas as pd
@@ -19,20 +23,37 @@ from text_model.text_preprocessing import _load_embedding_weights_glove, _paragr
 from image_model.im_model import load_batch_with_text, get_init_fn
 from datasets.convert_to_dataset import get_split_with_text
 import matplotlib.pyplot as plt
+from tensorflow.contrib.training.python.training.evaluation import StopAfterNEvalsHook, checkpoints_iterator
+
+import time
+
+from tensorflow.python.ops import state_ops
+from tensorflow.python.platform import tf_logging as logging
+from tensorflow.python.summary import summary
+from tensorflow.python.training import basic_session_run_hooks
+from tensorflow.python.training import checkpoint_management
+from tensorflow.python.training import evaluation
+from tensorflow.python.training import monitored_session
+from tensorflow.python.training import session_run_hook
+from tensorflow.python.training import training_util
+from tensorflow.python.ops import math_ops
+
+get_or_create_eval_step = evaluation._get_or_create_eval_step
+
 
 _POST_SIZE = 50
 _CONFIG = {'mode': 'train',
-		   'dataset_dir': 'data',
-		   'text_dir': 'text_model',
-		   'emb_dir': 'embedding_weights',
-		   'filename': 'glove.6B.50d.txt',
-		   'initial_lr': 1e-3,
-		   'decay_factor': 0.3,
-		   'batch_size': 64,
-		   'im_features_size': 256,
-		   'rnn_size': 1024,
-		   'final_endpoint': 'Mixed_5c',
-		   'fc_size': 512}
+			 'dataset_dir': 'data',
+			 'text_dir': 'text_model',
+			 'emb_dir': 'embedding_weights',
+			 'filename': 'glove.6B.50d.txt',
+			 'initial_lr': 1e-3,
+			 'decay_factor': 0.3,
+			 'batch_size': 64,
+			 'im_features_size': 256,
+			 'rnn_size': 1024,
+			 'final_endpoint': 'Mixed_5c',
+			 'fc_size': 512}
 
 
 class DeepSentiment():
@@ -58,10 +79,11 @@ class DeepSentiment():
 		self.lr_rate_assign = self.learning_rate.assign(
 			self.lr_rate_placeholder)
 
-		self.dataset = get_split_with_text(mode, dataset_dir)
+		# self.dataset = get_split_with_text(mode, dataset_dir, tfrecords_subdir="tfrecords")
+		self.dataset = get_split_with_text("train", dataset_dir, tfrecords_subdir="tfrecords_ol")
 		image_size = inception_v1.default_image_size
-		images, _, texts, seq_lens, self.labels, self.post_ids, self.days = load_batch_with_text(self.dataset, batch_size,
-			height=image_size, width=image_size)
+		images, _, texts, seq_lens, self.labels, self.post_ids, self.days = load_batch_with_text(
+			self.dataset, batch_size, height=image_size, width=image_size, shuffle=False)
 
 		# Create the model, use the default arg scope to configure the batch
 		# norm parameters.
@@ -116,7 +138,7 @@ class DeepSentiment():
 		self.logits = tf.matmul(dense_layer_relu, W_softmax) + b_softmax
 
 
-def train_deep_sentiment(checkpoints_dir, train_dir, num_steps):
+def train_deep_sentiment(checkpoints_dir, train_dir, num_steps, model_name=None):
 	"""Fine tune the inception model, retraining the last layer.
 
 	Parameters:
@@ -176,17 +198,20 @@ def train_deep_sentiment(checkpoints_dir, train_dir, num_steps):
 		final_loss = slim.learning.train(
 			train_op,
 			logdir=train_dir,
-			init_fn=get_init_fn(checkpoints_dir),
+			init_fn=get_init_fn(checkpoints_dir, model_name=model_name),
 			save_interval_secs=600,
 			save_summaries_secs=600,
 			train_step_fn=train_step_fn,
 			number_of_steps=num_steps)
 
+	return model
+
 	print('Finished training. Last batch loss {0:.3f}'.format(final_loss))
 
 
 def evaluate_deep_sentiment(checkpoint_dir, log_dir, mode, num_evals):
-	"""Visualise results with: tensorboard --logdir=logdir. Now has train/validation curves on the same plot
+	"""Visualise results with: tensorboard --logdir=logdir.
+	Now has train/validation curves on the same plot
 
 	Parameters:
 		checkpoint_dir: Checkpoint of the saved model during training.
@@ -200,8 +225,11 @@ def evaluate_deep_sentiment(checkpoint_dir, log_dir, mode, num_evals):
 		model = DeepSentiment(config)
 
 		# Accuracy metrics
-		accuracy = slim.metrics.streaming_accuracy(tf.cast(model.labels, tf.int32),
-												   tf.cast(tf.argmax(model.logits, 1), tf.int32))
+		softmax_op = tf.nn.softmax(model.logits, axis=1)
+		print_post_id_op = tf.convert_to_tensor(model.post_ids)
+		accuracy = slim.metrics.streaming_accuracy(
+			tf.cast(model.labels, tf.int32),
+			tf.cast(tf.argmax(model.logits, 1), tf.int32))
 
 		# Choose the metrics to compute:
 		names_to_values, names_to_updates = slim.metrics.aggregate_metric_map({
@@ -212,16 +240,54 @@ def evaluate_deep_sentiment(checkpoint_dir, log_dir, mode, num_evals):
 			tf.summary.scalar(metric_name, metric_value)
 
 		log_dir = os.path.join(log_dir, mode)
+		# slim.evaluation.evaluation_loop(
+		# 	'',
+		# 	checkpoint_dir,
+		# 	log_dir,
+		# 	num_evals=num_evals,
+		# 	eval_op=names_to_updates.values()
+		# )
 
 		# Evaluate every eval_interval_secs secs or if not specified,
 		# every time the checkpoint_dir changes
 		# tf.get_variable variables are also restored
-		slim.evaluation.evaluation_loop(
+		# slim.evaluation.evaluate_repeatedly(
+		# output = slim.evaluation.evaluate_once(
+		# 	'',
+		# 	checkpoint_dir,
+		# 	log_dir,
+		# 	eval_op=[v, tf.cast(tf.equal(
+		# 		tf.cast(model.labels, tf.int32),
+		# 		tf.cast(tf.argmax(model.logits, 1), tf.int32)
+		# 	), tf.int32)],
+		# )
+		# print(output[0])
+		# evaluate_once(
+		# 	'',
+		# 	checkpoint_dir,
+		# 	scaffold=monitored_session.Scaffold(
+		# 		init_op=None, init_feed_dict=None, saver=None),
+		# 	eval_ops=[v, tf.cast(tf.equal(
+		# 		tf.cast(model.labels, tf.int32),
+		# 		tf.cast(tf.argmax(model.logits, 1), tf.int32)
+		# 	), tf.int32)],
+		# 	hooks=[StopAfterNEvalsHook(1)]
+		# )
+		evaluate_repeatedly(
 			'',
 			checkpoint_dir,
-			log_dir,
-			num_evals=num_evals,
-			eval_op=names_to_updates.values())
+			# log_dir,
+			# num_evals=num_evals,
+			scaffold=monitored_session.Scaffold(
+				init_op=None, init_feed_dict=None,
+				init_fn=None, saver=None),
+			eval_ops=[softmax_op, tf.cast(tf.equal(
+				tf.cast(model.labels, tf.int32),
+				tf.cast(tf.argmax(model.logits, 1), tf.int32)
+			), tf.int32), print_post_id_op],
+			hooks=[StopAfterNEvalsHook(5000)],
+			max_number_of_evaluations=1)
+		# eval_ops=tf.Print(list(names_to_updates.values()), [v]))
 
 
 def deprocess_image(np_image):
@@ -506,11 +572,11 @@ def word_most_relevant(top_words, num_classes, checkpoint_dir):
 			for i in range(nb_iter):
 				np_images = np.zeros((batch_size, image_size, image_size, 3))
 				np_texts = np.ones((batch_size, _POST_SIZE),
-								   dtype=np.int32) * (vocab_size - 1)
+									 dtype=np.int32) * (vocab_size - 1)
 				np_texts[:, 0] = top_words[i*batch_size: (i+1)*batch_size]
 				np_seq_lens = np.ones(batch_size, dtype=np.int32)
 				scores.append(session.run(logits, feed_dict={
-							  images: np_images, texts: np_texts, seq_lens: np_seq_lens}))
+								images: np_images, texts: np_texts, seq_lens: np_seq_lens}))
 	scores = np.vstack(scores)
 	np.save('data/top_words_scores.npy', scores)
 	np.save('data/top_words.npy', top_words)
@@ -716,7 +782,7 @@ def oasis_evaluation(checkpoint_dir, num_classes):
 			im_path = 'data/oasis/images/' + name.strip() + '.jpg'
 			one_im = imread(im_path)
 			one_im = imresize(one_im, ((image_size, image_size, 3)))[
-							  :, :, :3]  # to get rid of alpha channel
+								:, :, :3]  # to get rid of alpha channel
 			return one_im
 
 		df_oasis['image'] = df_oasis['Theme'].map(lambda x: load_image(x))
@@ -728,7 +794,7 @@ def oasis_evaluation(checkpoint_dir, num_classes):
 		word_to_id = dict(zip(vocabulary, range(len(vocabulary))))
 		df_oasis['text_list'], df_oasis['text_len'] = zip(*df_oasis['Theme'].map(lambda x:
 																				_paragraph_to_ids(x, word_to_id,
-																								  _POST_SIZE, emotions='')))
+																									_POST_SIZE, emotions='')))
 		with monitored_session.MonitoredSession(
 			session_creator=session_creator, hooks=None) as session:
 
@@ -751,3 +817,131 @@ def oasis_evaluation(checkpoint_dir, num_classes):
 	scores = np.vstack(scores)
 	np.save('data/oasis_logits.npy', scores)
 	return scores
+
+def evaluate_repeatedly(
+	master='',
+	checkpoint_dir='',
+	scaffold=None,
+	eval_ops=None,
+	feed_dict=None,
+	final_ops=None,
+	final_ops_feed_dict=None,
+	eval_interval_secs=60,
+	hooks=None,
+	config=None,
+	max_number_of_evaluations=1,
+	timeout=None,
+	timeout_fn=None):
+	"""Repeatedly searches for a checkpoint in `checkpoint_dir` and evaluates it.
+	During a single evaluation, the `eval_ops` is run until the session is
+	interrupted or requested to finish. This is typically requested via a
+	`tf.contrib.training.StopAfterNEvalsHook` which results in `eval_ops` running
+	the requested number of times.
+	Optionally, a user can pass in `final_ops`, a single `Tensor`, a list of
+	`Tensors` or a dictionary from names to `Tensors`. The `final_ops` is
+	evaluated a single time after `eval_ops` has finished running and the fetched
+	values of `final_ops` are returned. If `final_ops` is left as `None`, then
+	`None` is returned.
+	One may also consider using a `tf.contrib.training.SummaryAtEndHook` to record
+	summaries after the `eval_ops` have run. If `eval_ops` is `None`, the
+	summaries run immediately after the model checkpoint has been restored.
+	Note that `evaluate_once` creates a local variable used to track the number of
+	evaluations run via `tf.contrib.training.get_or_create_eval_step`.
+	Consequently, if a custom local init op is provided via a `scaffold`, the
+	caller should ensure that the local init op also initializes the eval step.
+	Args:
+		checkpoint_dir: The directory where checkpoints are stored.
+		master: The address of the TensorFlow master.
+		scaffold: An tf.train.Scaffold instance for initializing variables and
+			restoring variables. Note that `scaffold.init_fn` is used by the function
+			to restore the checkpoint. If you supply a custom init_fn, then it must
+			also take care of restoring the model from its checkpoint.
+		eval_ops: A single `Tensor`, a list of `Tensors` or a dictionary of names
+			to `Tensors`, which is run until the session is requested to stop,
+			commonly done by a `tf.contrib.training.StopAfterNEvalsHook`.
+		feed_dict: The feed dictionary to use when executing the `eval_ops`.
+		final_ops: A single `Tensor`, a list of `Tensors` or a dictionary of names
+			to `Tensors`.
+		final_ops_feed_dict: A feed dictionary to use when evaluating `final_ops`.
+		eval_interval_secs: The minimum number of seconds between evaluations.
+		hooks: List of `tf.train.SessionRunHook` callbacks which are run inside the
+			evaluation loop.
+		config: An instance of `tf.ConfigProto` that will be used to
+			configure the `Session`. If left as `None`, the default will be used.
+		max_number_of_evaluations: The maximum times to run the evaluation. If left
+			as `None`, then evaluation runs indefinitely.
+		timeout: The maximum amount of time to wait between checkpoints. If left as
+			`None`, then the process will wait indefinitely.
+		timeout_fn: Optional function to call after a timeout.  If the function
+			returns True, then it means that no new checkpoints will be generated and
+			the iterator will exit.  The function is called with no arguments.
+	Returns:
+		The fetched values of `final_ops` or `None` if `final_ops` is `None`.
+	"""
+	# import pdb; pdb.set_trace()
+	eval_step = get_or_create_eval_step()
+
+	# Prepare the run hooks.
+	hooks = hooks or []
+	if eval_ops is not None:
+		update_eval_step = state_ops.assign_add(eval_step, 1)
+
+		for h in hooks:
+			if isinstance(h, StopAfterNEvalsHook):
+				h._set_evals_completed_tensor(update_eval_step)  # pylint: disable=protected-access
+
+		if isinstance(eval_ops, dict):
+			eval_ops['update_eval_step'] = update_eval_step
+		elif isinstance(eval_ops, (tuple, list)):
+			eval_ops = list(eval_ops) + [update_eval_step]
+		else:
+			eval_ops = [eval_ops, update_eval_step]
+
+	final_ops_hook = basic_session_run_hooks.FinalOpsHook(
+		final_ops,
+		final_ops_feed_dict)
+	hooks.append(final_ops_hook)
+
+	num_evaluations = 0
+	for checkpoint_path in checkpoints_iterator(
+			checkpoint_dir,
+			min_interval_secs=eval_interval_secs,
+			timeout=timeout,
+			timeout_fn=timeout_fn):
+		session_creator = monitored_session.ChiefSessionCreator(
+				scaffold=scaffold,
+				checkpoint_filename_with_path=checkpoint_path,
+				master=master,
+				config=config)
+
+		total = 0
+		with open('filter_value.csv', 'w+') as file:
+			writer = csv.writer(file, delimiter=',')
+			with monitored_session.MonitoredSession(
+					session_creator=session_creator, hooks=hooks) as session:
+				logging.info('Starting evaluation at ' + time.strftime(
+						'%Y-%m-%d-%H:%M:%S', time.gmtime()))
+				if eval_ops is not None:
+					while not session.should_stop():
+						output = session.run(eval_ops, feed_dict)
+						maxes = [str(max(row)) for row in output[0]]
+						correctness = [str(val) for val in output[1]]
+						post_ids = [str(val) for val in output[2]]
+						for row in zip(post_ids, maxes, correctness):
+							writer.writerow(row)
+						# print(output[0][1])
+						total += len(output[0])
+						print(sum(output[0][1]), total)
+						if total > 2000000:
+							return
+
+				logging.info('Finished evaluation at ' + time.strftime(
+						'%Y-%m-%d-%H:%M:%S', time.gmtime()))
+		num_evaluations += 1
+		print(total)
+		if (
+			max_number_of_evaluations is not None and
+			num_evaluations >= max_number_of_evaluations):
+			return final_ops_hook.final_ops_values
+
+	return final_ops_hook.final_ops_values
