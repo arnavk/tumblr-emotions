131ffacc74885fa3faa531d11cae2a386032ace4
Arnav Kumar <arnav@arnavkumar.com>
2018-11-30 17:20:54 -0500

    Make text processing script work with our data

--- text_model/text_preprocessing.py
+++ text_model/text_preprocessing.py
@@ -1,6 +1,6 @@
 import os
 import re
-#import gensim
+# import gensim
 
 import numpy as np
 import pandas as pd
@@ -12,12 +12,12 @@ _MIN_ENGLISH_WORDS_IN_POST = 5
 
 def _load_embedding_weights_glove(text_dir, emb_dir, filename):
     """Load the word embedding weights from a pre-trained model.
-    
+
     Parameters:
         text_dir: The directory containing the text model.
         emb_dir: The subdirectory containing the weights.
         filename: The name of that text file.
-        
+
     Returns:
         vocabulary: A list containing the words in the vocabulary.
         embedding: A numpy array of the weights.
@@ -36,12 +36,12 @@ def _load_embedding_weights_glove(text_dir, emb_dir, filename):
 
 def _load_embedding_weights_word2vec(text_dir, emb_dir, filename):
     """Load the word embedding weights from a pre-trained model.
-    
+
     Parameters:
         text_dir: The directory containing the text model.
         emb_dir: The subdirectory containing the weights.
         filename: The name of the binary file.
-        
+
     Returns:
         vocabulary: A list containing the words in the vocabulary.
         embedding: A numpy array of the weights.
@@ -53,11 +53,11 @@ def _load_embedding_weights_word2vec(text_dir, emb_dir, filename):
     print('Finished loading word embedding weights.')
     return vocabulary, embedding
 
-def _str_list_to_set(str_list):
+def _str_list_to_set(str_list, delimiter='|'):
     """Convert a string representation of a list such as '[happy, sun, outdoors]'
        to a set of strings {'happy', 'sun', 'outdoors'}
     """
-    output = str_list[1:-1].split(',')
+    output = str_list[1:-1].split(delimiter)
     output = set([x.strip() for x in output])
     return output
 
@@ -118,9 +118,14 @@ def preprocess_df(text_dir, emb_dir, filename, emb_name, emotions, post_size):
 
     columns = ['id', 'post_url', 'type', 'timestamp', 'date', 'tags', 'liked',
                'note_count', 'photo', 'text', 'search_query']
+    # columns = [
+    #     'num', 'id', 'post_url', 'date', 'liked', 'type', 'timestamp',
+    #     'tags', 'photo', 'caption', 'search_query', 'img_file_name'
+    # ]
     df_all = pd.DataFrame(columns=columns)
     for emotion in emotions:
         path = os.path.join('data', emotion + '.csv')
+        # Get all posts for that emotion
         df_emotion = _df_with_hashtag_in_post(pd.read_csv(path, encoding='utf-8'), emotion)
         df_all = pd.concat([df_all, df_emotion]).reset_index(drop=True)
 
@@ -129,7 +134,7 @@ def preprocess_df(text_dir, emb_dir, filename, emb_name, emotions, post_size):
     df_all =  df_all.loc[mask, :].reset_index(drop=True)
 
     # Map text to ids
-    df_all['text_list'], df_all['text_len'] = zip(*df_all['text'].map(lambda x: 
+    df_all['text_list'], df_all['text_len'] = zip(*df_all['text'].map(lambda x:
         _paragraph_to_ids(x, word_to_id, post_size, emotions)))
 
     # Binarise emotions
@@ -148,26 +153,27 @@ def preprocess_one_df(vocabulary, embedding, emotion, post_size):
     vocab_size, embedding_dim = embedding.shape
     word_to_id = dict(zip(vocabulary, range(vocab_size)))
     # Unknown words = vector with zeros
-    #embedding = np.concatenate([embedding, np.zeros((1, embedding_dim))])
+    # embedding = np.concatenate([embedding, np.zeros((1, embedding_dim))])
 
-    path = os.path.join('data', emotion + '.csv')
-    df_emotion = _df_with_hashtag_in_post(pd.read_csv(path, encoding='utf-8'), emotion)
-
+    # path = os.path.join('data', emotion + '.csv')
+    path = os.path.join('data', 'shuffled_train.csv')
+    df_emotion = _df_with_hashtag_in_post(
+        pd.read_csv(path, encoding='utf-8'), emotion)
+    # All posts with emotion in it
     vocab_set = set(vocabulary)
-    mask = df_emotion['text'].map(lambda x: _is_valid_text(x, vocab_set))
+    mask = df_emotion['caption'].map(lambda x: _is_valid_text(x, vocab_set))
     df_emotion =  df_emotion.loc[mask, :].reset_index(drop=True)
 
     emotions = ['happy', 'sad', 'angry', 'scared', 'disgusted', 'surprised']
     # Map text to ids
-    df_emotion['text_list'], df_emotion['text_len'] = zip(*df_emotion['text'].map(lambda x: 
+    df_emotion['text_list'], df_emotion['text_len'] = zip(*df_emotion['caption'].map(lambda x:
         _paragraph_to_ids(x, word_to_id, post_size, emotions)))
 
     # Binarise emotions
-    #emotion_dict = dict(zip(emotions, range(len(emotions))))
-    #df_all['search_query'] =  df_all['search_query'].map(emotion_dict)
+    # emotion_dict = dict(zip(emotions, range(len(emotions))))
+    # df_all['search_query'] =  df_all['search_query'].map(emotion_dict)
 
     # Add <ukn> word to dictionary
-    #word_to_id['<ukn>'] = vocab_size
+    # word_to_id['<ukn>'] = vocab_size
 
-    return df_emotion#, word_to_id, embedding
-
+    return df_emotion  # , word_to_id, embedding
